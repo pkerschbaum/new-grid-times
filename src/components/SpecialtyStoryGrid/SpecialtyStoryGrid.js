7fd30b4e2110faf72b9bc11bc60265376fe50422
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-09-28 12:02:53 +0200

    feat: Exercise 4: Specialty Story grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { COLORS, QUERIES } from '../../constants';
 
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
@@ -36,7 +37,9 @@ const SpecialtyStoryGrid = () => {
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+            <SportStory key={data.id}>
+              <MiniStory {...data} />
+            </SportStory>
           ))}
         </SportsStories>
       </SportsSection>
@@ -47,14 +50,46 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+  grid-template-columns: minmax(0, 1fr);
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid ${COLORS.gray[300]};
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(170px, 1fr));
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(170px, 1fr));
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    overflow-x: auto;
+  }
+`;
+
+const SportStory = styled.div`
+  @media ${QUERIES.tabletAndUp} {
+    flex-basis: 220px;
+    flex-shrink: 0;
+  }
+`;
 
 export default SpecialtyStoryGrid;
